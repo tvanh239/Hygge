881e4468eb224219130eed837fada99291d8407a
Vietanh239 <72076528+Vietanh239@users.noreply.github.com>
2024-01-14 21:40:07 +0700

    add comment

--- Service/AvatarService.cs
+++ Service/AvatarService.cs
@@ -17,9 +17,9 @@ namespace Hygge.Service
     /// <summary>Create avatar</summary>
     public class AvatarService
     {
-
+        /// <summary>Url of avatar</summary>
         public string? _url_avatar { get; set; }
-
+        /// <summary>the list of color</summary>
         private List<string> _BackgroundColours;
         private readonly IWebHostEnvironment _webHostEnvironment;
         public AvatarService(IWebHostEnvironment webHostEnvironment) {
