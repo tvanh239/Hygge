46fb3673dfe8c5f65772c313b326ee92f8ea8d90
Vietanh239 <72076528+Vietanh239@users.noreply.github.com>
2024-01-28 13:15:03 +0700

    add dialog

--- wwwroot/js/login.js
+++ wwwroot/js/login.js
@@ -1,1 +1,17 @@
 ﻿
+document.getElementById('meetingBefore').addEventListener('click', function () {
+    document.getElementById('meetingBefore').style.display = 'none';
+    document.getElementById('meetingAfter').style.display = '';
+});
+
+// When zoom-code is none, join-button is not active
+function CheckCodeJoin() {
+    var inputField = document.getElementById('inputCode');
+    var submitButton = document.getElementById('joinBtn');
+    if (inputField.value.trim() !== '') {
+        submitButton.removeAttribute('disabled');
+    } else {
+        submitButton.setAttribute('disabled', 'true');
+    }
+
+};
---
+++ wwwroot/js/user.js
@@ -0,0 +1,11 @@
+﻿var avaUser = document.getElementById("avatarImg");
+
+avaUser.addEventListener("click", function () {
+    var dialogInfo = document.getElementById("personalInfo");
+    dialogInfo.showModal();
+});
+
+document.getElementById("closeInfo").addEventListener("click", function () {
+    var dialogInfo = document.getElementById("personalInfo");
+    dialogInfo.close();
+});
