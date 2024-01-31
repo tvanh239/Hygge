122d7881a65882d0b34d6b3ef70feaee5b671fe4
Vietanh239 <72076528+Vietanh239@users.noreply.github.com>
2024-01-31 21:35:24 +0700

    add dialog icon

--- wwwroot/js/user.js
+++ wwwroot/js/user.js
@@ -1,8 +1,11 @@
 ﻿var avaUser = document.getElementById("avatarImg");
+var settingMenu = document.getElementById("settingMenu");
+
+///----------------------------- Dialog for user avatar　↓----------------------------
+
 avaUser.addEventListener("click", function () {
     var dialogInfo = document.getElementById("personalInfo");
     dialogInfo.showModal();
-    document.addEventListener('click', handleClickOutside);
 });
 
 document.getElementById("closeInfo").addEventListener("click", function () {
@@ -12,8 +15,30 @@ document.getElementById("closeInfo").addEventListener("click", function () {
 
 function handleClickOutside(event) {
     var dialogInfo = document.getElementById("personalInfo");
-    if (!dialogInfo.contains(event.target) && event.target !== document.getElementById('dialog') && event.target !== document.getElementsByClassName("image-avatar")[0]) {
+    if (!dialogInfo.contains(event.target) && event.target != document.getElementsByClassName("image-avatar")[0]) {
         // Click is outside the dialog, so close it
         dialogInfo.close();
     }
+    var menuDialog = document.getElementById("menuIcon");
+    if (!menuDialog.contains(event.target) && event.target != document.getElementsByClassName("btn-setting")[0]) {
+        // Click is outside the dialog, so close it
+        settingMenu.classList.remove('card-active') ;
+        menuDialog.close();
+    }
 }
+
+///-------------------------------Dialog for user avatar　↑------------------------------
+
+
+
+///----------------------------- Dialog for menu icon ↓--------------------------------
+settingMenu.addEventListener("click", function () {
+    this.className = this.className + ' card-active';
+    var dialogInfo = document.getElementById("menuIcon");
+    dialogInfo.showModal();
+    
+});
+
+
+document.addEventListener('click', handleClickOutside);
+///------------------------------Dialog for menu icon ↑--------------------------------
